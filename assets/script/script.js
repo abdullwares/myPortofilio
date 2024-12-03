659a9181e3331e833308ee61a33d5dc807cd7d7e
Wares Arab <waresarab10@gmail.com>
2024-12-03 19:02:21 +0430

    my First Commit

---
+++ assets/script/script.js
@@ -0,0 +1,25 @@
+
+
+// function () {
+//      "use strict";
+ 
+//      // Navbar on scrolling
+//      $(window).scroll(function () {
+//          if ($(this).scrollTop() > 200) {
+//              $('.navbar').fadeIn('slow').css('display', 'flex');
+//          } else {
+//              $('.navbar').fadeOut('slow').css('display', 'none');
+//          }
+//      });
+ 
+
+
+
+// // Skills
+//   $('.skill').waypoint(function () {
+//      $('.progress .progress-bar').each(function () {
+//          $(this).css("width", $(this).attr("aria-valuenow") + '%');
+//      });
+//  }, {offset: '80%'});
+
+
